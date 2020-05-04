7ceef375d0369ddb70d361faa9c8d68f4edbc32e
mikwon_roh <yearn45@gmail.com>
2020-05-04 13:20:38 +0900

    test:  animation test

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
       </div>
       <div class="section">
         Second section ...
+        <div id="right-squre" style="position:absolute;top:0;right:-100%;width:20%;height:100%;z-index:1;bottom:0;background-color:green"></div>
         <div id="circle2" style="display:block;width:20px;height:20px;border-radius:500em;background-color:#123"></div>
       </div>
       <div class="section">
@@ -44,6 +45,7 @@ export default {
           gsap.to("#circle", { x: 500, duration: 3 });
           break;
         case anchors[1]:
+          gsap.to("#right-squre", 1, { right: 0 });
           gsap.to("#circle2", { x: 500, duration: 3 });
           break;
       }
@@ -53,4 +55,8 @@ export default {
 </script>
 <style lang="scss">
 @import "@/styles/global.scss";
+
+.section {
+  position: relative;
+}
 </style>
