c181ee16841893b8c65b744f4bc036988bccf762
mikwon_roh <yearn45@gmail.com>
2020-05-04 11:13:13 +0900

    chore: fullpage 추가

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <full-page ref="fullpage" :options="options" id="fullpage">
+      <div class="section">
+        First section ...
+      </div>
+      <div class="section">
+        Second section ...
+      </div>
+      <div class="section">
+        Third section ...
+      </div>
+    </full-page>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      options: {
+        licenseKey: "YOUR_KEY_HEERE",
+        menu: "#menu",
+        anchors: ["page1", "page2", "page3"],
+        sectionsColor: ["#41b883", "#ff5f45", "#0798ec"]
+      }
+    };
+  }
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -4,10 +4,16 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+// import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
+// import './fullpage.scrollHorizontally.min' // Optional. When using fullpage extensions
+import VueFullPage from "vue-fullpage.js";
+
 Vue.config.productionTip = false;
+
+Vue.use(VueFullPage);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount("#app");
