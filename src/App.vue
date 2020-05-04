02632fcf25d9b6c5961efc0c79f16653b84d3e16
mikwon_roh <yearn45@gmail.com>
2020-05-04 12:00:34 +0900

    test:  animation test

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@
     <full-page :options="options">
       <div class="section">
         First section ...
-        <div class="circle" style="display:block;width:20px;height:20px;border-radius:500em;background-color:#fff"></div>
+        <div id="circle" style="display:block;width:20px;height:20px;border-radius:500em;background-color:#fff"></div>
       </div>
       <div class="section">
         Second section ...
+        <div id="circle2" style="display:block;width:20px;height:20px;border-radius:500em;background-color:#123"></div>
       </div>
       <div class="section">
         Third section ...
@@ -21,13 +22,15 @@
 <script>
 import { gsap } from "gsap";
 
+const anchors = ["page1", "page2", "page3"];
+
 export default {
   data() {
     return {
       options: {
         // licenseKey: "YOUR_KEY_HEERE",
         menu: "#menu",
-        anchors: ["page1", "page2", "page3"],
+        anchors: anchors,
         sectionsColor: ["#41b883", "#ff5f45", "#0798ec"],
         afterLoad: this.afterLoad
       }
@@ -35,8 +38,15 @@ export default {
   },
   methods: {
     afterLoad(origin, destination, direction) {
-      console.log(origin, destination, direction);
-      gsap.to(".circle", { x: 500, duration: 3 });
+      console.log(origin, destination.index, direction);
+      switch (destination.anchor) {
+        case anchors[0]:
+          gsap.to("#circle", { x: 500, duration: 3 });
+          break;
+        case anchors[1]:
+          gsap.to("#circle2", { x: 500, duration: 3 });
+          break;
+      }
     }
   }
 };
